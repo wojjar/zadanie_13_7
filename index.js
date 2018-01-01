89faace34d9a6b8497f32770fc27345144cc48d3
wojjar <jaroslaw@wojcik.hub.pl>
2018-01-01 20:59:29 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-    fs.readdir('../zadanie_13_7', 'utf-8', function(err, data) {
+    fs.readdir('./zadanie_13_7', 'utf-8', function(err, data) {
         console.log(data);
         fs.writeFile('filesList.txt',data , (err) => {
             if (err) throw err;
